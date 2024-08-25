6dfe193825e9a254b03e7f20917901b5596e6219
Sumit Mishra <sm0141136@gmail.com>
2024-08-25 15:28:32 +0530

    pahala program

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Item from './Components/Item';
+import ItemDate from './Components/ItemDate';
+import Card from './Components/Card';
 
 function App() {
+  const response=[
+    {
+      itemName:"Nirma",
+      itemDate:"20",
+      itemMonth:"June",
+      itemyear:"1998"
+    },
+    {
+      itemName:"Nirma2",
+      itemDate:"202",
+      itemMonth:"June2",
+      itemyear:"19982"
+    },
+    {
+      itemName:"Nirma3",
+      itemDate:"203",
+      itemMonth:"June3",
+      itemyear:"19983"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <Card>
+          <Item name={response[0].itemName}>
+            hello ji mai hu tumhari first items
+            </Item>
+            <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemyear}></ItemDate>
+            <Item name={response[1].itemName}></Item>
+            <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemyear}></ItemDate>
+            <Item name={response[2].itemName}></Item>
+            <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemyear}></ItemDate>
+        </Card>
+     </div>
+  
   );
 }
 
